8f6ce32d28d56ac18560a40e86513e308a274703
Marcelo <marcelo.tissoni@gmail.com>
2022-06-17 20:10:14 -0300

    coffee

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -169,10 +169,22 @@ export default function Instructions() {
 
     
     <Styles>
-      <h2>Donate</h2>
+      <h2>Password Bookmarklet</h2>
       <ol>
       <p>Why buy me a coffee? No third-party ads, no affiliate links, no tracking cookies. Thanks.</p>
-     <a href="https://ko-fi.com/passwordgenlive"><img src="images/kofi.png" alt="Buy me a coffee"</a>
+
+<button class="bookmark">
+    <div class="icon">
+        <svg viewBox="0 0 36 36">
+            <path class="filled" d="M26 6H10V18V30C10 30 17.9746 23.5 18 23.5C18.0254 23.5 26 30 26 30V18V6Z" />
+            <path class="default" d="M26 6H10V18V30C10 30 17.9746 23.5 18 23.5C18.0254 23.5 26 30 26 30V18V6Z" />
+            <path class="corner" d="M10 6C10 6 14.8758 6 18 6C21.1242 6 26 6 26 6C26 6 26 6 26 6H10C10 6 10 6 10 6Z" />
+        </svg>
+    </div>
+    <span><a href="https://ko-fi.com/passwordgenlive">Buy Me a Coffee</a></span>
+</button>
+
+
       </ol>
     </Styles>
   );
