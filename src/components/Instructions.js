84996777674a5de77ce42b6f831a40219f5aa700
Marcelo <marcelo.tissoni@gmail.com>
2022-06-17 20:00:21 -0300

    immm

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -174,7 +174,8 @@ export default function Instructions() {
 
 
 
-      <a href='https://ko-fi.com/R6R1DBPCQ'><img height='36' style='border:0px;height:36px;' src='https://cdn.ko-fi.com/cdn/kofi3.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
+      <a href="https://ko-fi.com/passwordgenlive"><img src="images/kofi.png" alt="Buy me a coffee"></a>
+
 
 
       
