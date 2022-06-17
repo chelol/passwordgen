4836203d3fc8d0edb19feb6cc8c01fedcb49c0d2
Marcelo <marcelo.tissoni@gmail.com>
2022-06-17 19:18:31 -0300

    new

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -169,24 +169,12 @@ export default function Instructions() {
 
     
     <Styles>
-      <h2>Password Bookmarklet</h2>
+      <h2>Donate</h2>
       <ol>
-        <p>Drag this bookmarklet to your browser bookmark bar. Click it for a 32 character (letters and numbers only) password.
+        <p>.
 </p>
 
-
-
-<button class="bookmark">
-    <div class="icon">
-        <svg viewBox="0 0 36 36">
-            <path class="filled" d="M26 6H10V18V30C10 30 17.9746 23.5 18 23.5C18.0254 23.5 26 30 26 30V18V6Z" />
-            <path class="default" d="M26 6H10V18V30C10 30 17.9746 23.5 18 23.5C18.0254 23.5 26 30 26 30V18V6Z" />
-            <path class="corner" d="M10 6C10 6 14.8758 6 18 6C21.1242 6 26 6 26 6C26 6 26 6 26 6H10C10 6 10 6 10 6Z" />
-        </svg>
-    </div>
-    <span><a href="https://passwordgen.live">PasswordGen</a></span>
-</button>
-
+<script type='text/javascript' src='https://storage.ko-fi.com/cdn/widget/Widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('Support Me on Ko-fi', '#29abe0', 'R6R1DBPCQ');kofiwidget2.draw();</script> 
 
       </ol>
     </Styles>
